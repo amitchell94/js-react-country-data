43729f1dbcb06beda8b41ba5a0629292586a01f8
Andy Mitchell <andymitchell1@live.co.uk>
2021-08-19 00:14:04 +0200

    Completed exercise 2.13, which adds a show button for each country.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import axios from 'axios'
 import {useEffect, useState} from "react";
 
-const Countries = ({countries}) => {
+const Countries = ({countries, onClick}) => {
     if (countries.length > 10) {
         return (<div>Too many matches, specify another filter</div>)
     } else if (countries.length === 1) {
@@ -11,10 +11,11 @@ const Countries = ({countries}) => {
             <div>
                 <h2>{country.name}</h2>
                 capital {country.capital}
+                <br/>
                 population {country.population}
                 <h3>Languages</h3>
                 <ul>
-                    {country.languages.map((language,i) =>
+                    {country.languages.map((language, i) =>
                         <div key={i}>{language.name}</div>
                     )}
                 </ul>
@@ -24,7 +25,9 @@ const Countries = ({countries}) => {
         return (
             <ul>
                 {countries.map((country, i) =>
-                    <div key={i}>{country.name}</div>
+                    <div key={i}>{country.name}
+                        <button onClick={() => onClick(country.name)}>show</button>
+                    </div>
                 )}
             </ul>
         )
@@ -53,10 +56,15 @@ function App() {
     const handleFilterChange = (event) => {
         setFilter(event.target.value)
     }
+
+    const setFilterValue = (value) => {
+        setFilter(value)
+    }
+
     return (
         <div>
             find countries <input value={filter} onChange={handleFilterChange}/>
-            <Countries countries={countries} />
+            <Countries countries={countries} onClick={setFilterValue}/>
         </div>
     );
 }
