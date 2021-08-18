267a4e29780e8a701cdc6b0defff757d70c38716
Andy Mitchell <andymitchell1@live.co.uk>
2021-08-18 23:49:40 +0200

    First commit, search engine working, need to implement specific country data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from "react";
+
+const Countries = ({countries}) => {
+    if (countries.length > 10) {
+        return (<div>Too many matches, specify another filter</div>)
+    } else if (countries.length === 1) {
+        return (<div>Do this later</div>)
+    } else {
+        return (
+            <ul>
+                {countries.map((country, i) =>
+                    <div key={i}>{country.name}</div>
+                )}
+            </ul>
+        )
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([])
+    const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+        let url = ""
+        if (filter.length > 0) {
+            url = `https://restcountries.eu/rest/v2/name/${filter}`
+        } else {
+            url = `https://restcountries.eu/rest/v2/all`
+        }
+
+        axios
+            .get(url)
+            .then(response => {
+                setCountries(response.data)
+            })
+    }, [filter])
+
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+    return (
+        <div>
+            find countries <input value={filter} onChange={handleFilterChange}/>
+            <Countries countries={countries} />
+        </div>
+    );
 }
 
 export default App;
